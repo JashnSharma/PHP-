73bd7686ec4331e1cbda70feb7a2664efec221d3
JashnSharma <jashnsharma18@gmail.com>
2025-01-20 20:46:09 +0530

    Update armstrong.php

--- armstrong.php
+++ armstrong.php
@@ -17,6 +17,8 @@ if ($sum == $originalNumber) {
     echo "$originalNumber is an Armstrong number.";
 } else {
     echo "$originalNumber is not an Armstrong number.";
-    echo("This program is written and executed by Jashn Sharma (2220100247)");
 }
+    echo"<br>";
+    echo("This program is written and executed by Jashn Sharma (2220100247)");
+
 ?>
