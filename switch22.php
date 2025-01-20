80299e983671126a26ca8cd18f2be4340a7d55c0
JashnSharma <jashnsharma18@gmail.com>
2025-01-20 20:49:41 +0530

    Update switch22.php

--- switch22.php
+++ switch22.php
@@ -17,5 +17,6 @@
             echo "Your favorite color is neither red, blue, nor green!";
     }
     echo"<br>";
-echo("This program is written and executed by Diya Arora (2220100252)");
+echo("This program is written and executed by Jashn Sharma (2220100247)");
+
 ?>
