8d16493309916deb6ac4c555a5ad5bd3c9e1dd42
JashnSharma <jashnsharma18@gmail.com>
2025-01-20 20:55:25 +0530

    Delete square.php

--- square.php
+++
@@ -1,7 +0,0 @@
-<?php
-$number = 5; 
-$square = $number * $number;
-echo "The square of $number is: $square";
-echo"<br>";
-echo("This program is written and executed by Jashn Sharma (2220100247)");
-?>
