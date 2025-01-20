069bcdc7e19c0e2953e9960dbc0544635c33f0c1
JashnSharma <jashnsharma18@gmail.com>
2025-01-20 20:49:26 +0530

    Update stringoper16.php

--- stringoper16.php
+++ stringoper16.php
@@ -7,5 +7,6 @@
     $x .= $y;
     echo $x; 
  echo "<br>";
-echo("This program is written and executed by Diya Arora (2220100252)");
+echo("This program is written and executed by Jashn Sharma (2220100247)");
+
 ?>
