f3e410586643830cda0ece3635703de8c6e5b920
JashnSharma <jashnsharma18@gmail.com>
2025-04-11 11:27:37 +0530

    Update Open a Text File and Write Some Content 78.php

--- Open a Text File and Write Some Content 78.php
+++ Open a Text File and Write Some Content 78.php
@@ -1,7 +1,7 @@
 <?php
 $file = fopen("example.txt", "w"); // Open the file in write mode
 fwrite($file, "Hello, this is a sample text written into the file.\n");
-fwrite($file, "This program is written by Kritika ERP-067.");
+fwrite($file, "This program is written by Jashn ERP-048.");
 fclose($file);
 echo "File has been written successfully.";
 ?>
