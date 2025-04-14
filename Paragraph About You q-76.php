1a54f8c0456fde91d8b69fbb34a5e56c22811418
JashnSharma <jashnsharma18@gmail.com>
2025-04-15 01:45:52 +0530

    Update Paragraph About You q-76.php

--- Paragraph About You q-76.php
+++ Paragraph About You q-76.php
@@ -1,8 +1,8 @@
 <?php
 $file = fopen("about_me.txt", "w"); // Open the file in write mode
-$text = "My name is Kritika. I am passionate about cybersecurity and programming.
+$text = "My name is JASHN. I am passionate about cybersecurity and programming.
 I love exploring new technologies and working on projects that enhance my skills.\n";
-$text .= "This program is written by Kritika ERP-067.";
+$text .= "This program is written by JASHN ERP-048.";
 fwrite($file, $text);
 fclose($file);
 echo "File 'about_me.txt' has been created successfully.<br>";
