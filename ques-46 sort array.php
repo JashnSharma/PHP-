1f2634d27cf43995cf6a6fd071915968ed0b770f
JashnSharma <jashnsharma18@gmail.com>
2025-04-15 01:34:15 +0530

    Update ques-46 sort array.php

--- ques-46 sort array.php
+++ ques-46 sort array.php
@@ -10,5 +10,5 @@ echo "Sorted Names:<br>";
 foreach ($names as $name) {
     echo $name . "<br>";
 }
-echo"this program is written by kritika erp-067";
+echo "This Program is written by JASHN SHARMA, 2220100247";
 ?>
