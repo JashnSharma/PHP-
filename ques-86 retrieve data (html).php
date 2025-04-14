a483493a14dc96ef2a91f673897124fd261efc5c
JashnSharma <jashnsharma18@gmail.com>
2025-04-15 01:44:30 +0530

    Update ques-86 retrieve data (html).php

--- ques-86 retrieve data (html).php
+++ ques-86 retrieve data (html).php
@@ -31,6 +31,6 @@
         echo "0 results";
     }
     $conn->close();
-            echo "This Program is written by kritika, 0221BCA067"
+            echo "This Program is written by JASHN SHARMA, 2220100247";
 
 ?>
