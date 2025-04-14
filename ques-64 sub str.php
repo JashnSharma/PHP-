8f13d969b12608686012d395d705e30137f6c2d4
JashnSharma <jashnsharma18@gmail.com>
2025-04-15 01:36:23 +0530

    Update ques-64 sub str.php

--- ques-64 sub str.php
+++ ques-64 sub str.php
@@ -10,5 +10,5 @@ echo "Original Text: " . $text . "<br>";
 echo "Substring (from position 11, length 3): " . $substring . "<br>";
 
 // Add author line
-echo "This program is written by KRITIKA ERP-067";
+echo "This Program is written by JASHN SHARMA, 2220100247";
 ?>
