012c3caae127bb1404e3e13021b60839bb101d46
JashnSharma <jashnsharma18@gmail.com>
2025-04-15 01:40:58 +0530

    Update ques-74 delete cookie.php

--- ques-74 delete cookie.php
+++ ques-74 delete cookie.php
@@ -2,7 +2,7 @@
 
     // Deleting a cookie
     setcookie("username", "", time()-3600);
-echo "<p>This program is written by <strong>KRITIKA ERP-067</strong></p>";
+echo "<p>This program is written by <strong>JASHN ERP-048</strong></p>";
 
 
 ?>
