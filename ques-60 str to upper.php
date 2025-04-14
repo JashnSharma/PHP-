77cc344e474744194629cffa7647b7e02851cab8
JashnSharma <jashnsharma18@gmail.com>
2025-04-15 01:35:24 +0530

    Update ques-60 str to upper.php

--- ques-60 str to upper.php
+++ ques-60 str to upper.php
@@ -10,5 +10,5 @@ echo "Original Text: " . $text . "<br>";
 echo "Uppercase Text: " . $upperText . "<br>";
 
 // Add author line
-echo "This program is written by KRITIKA ERP-067";
+echo "This Program is written by JASHN SHARMA, 2220100247";
 ?>
